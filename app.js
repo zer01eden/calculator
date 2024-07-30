4d9473e2e379859d9c6f726ab6362bd0b669af1d
zero_jeden <zeroitjeden@gmail.com>
2024-07-30 23:25:28 +0200

    styles: clean & save

--- app.js
+++ app.js
@@ -3,9 +3,6 @@
 function add(a, b) {
 	return a + b;
 }
-// console.log(add(1, 5));
-// console.log(add(11, 56));
-
 var userNum1 = Number(prompt('add first number', 'write only number'));
 var userNum2 = Number(prompt('add your second number', 'write only number'));
 
