f8002b112d8488afd65565543c6349a791a51e0e
zero_jeden <zeroitjeden@gmail.com>
2024-08-03 16:32:29 +0200

    refactor: new function showResult  & cleaning code

--- app.js
+++ app.js
@@ -3,21 +3,27 @@
 //i try use IIFE
 
 (function () {
+	var numbersAmount = 3;
+
 	function add(a, b) {
 		return a + b;
 	}
+	function showResult() {
+		if (sum) {
+			alert(`your result is: ${sum}`);
+		} else {
+			if (sum === 0) {
+				alert(`your result is: ${sum}`);
+			} else {
+				alert(`you add wrong data, try again`);
+			}
+		}
+	}
+
 	var userNum1 = Number(prompt('add first number', 'write only number'));
 	var userNum2 = Number(prompt('add your second number', 'write only number'));
 
 	var sum = add(userNum1, userNum2);
 
-	if (sum) {
-		alert(`your result is: ${sum}`);
-	} else {
-		if (sum === 0) {
-			alert(`your result is: ${sum}`);
-		} else {
-			alert(`you add wrong data, try again`);
-		}
-	}
+	showResult();
 })();
